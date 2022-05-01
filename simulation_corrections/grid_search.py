f774938125f1057168eb443f87df3055bad920e1
Wei Deng <greyman@live.cn>
2022-05-01 19:55:05 -0400

    .

--- simulation_corrections/grid_search.py
+++ simulation_corrections/grid_search.py
@@ -7,26 +7,25 @@ import sys
 secure_random = random.SystemRandom()
 
 
-for _ in range(1):
+for _ in range(3):
     seed = str(random.randint(1, 10**5))
     sd = secure_random.choice([4])
-    #window = secure_random.choice([1, 3, 10, 30, 100, 300, 1000])
-    window = 1
+    window = secure_random.choice([3, 10, 30, 100, 300, 1000])
     if window == 1:
         correction = 0
     elif window == 3:
-        correction = secure_random.choice([0, 2.0, 2.2, 2.4, 2.0, 2.2, 2.4])
+        correction = secure_random.choice([1.8, 2, 2.2])
     elif window == 10:
-        correction = secure_random.choice([0, 2.0, 2.3, 2.6, 2.0, 2.3, 2.6])
+        correction = secure_random.choice([1.7, 2.0, 2.3])
     elif window == 30:
-        correction = secure_random.choice([0, 3.0, 3.3, 3.6, 3.0, 3.3, 3.6])
+        correction = secure_random.choice([2.7, 3.0, 3.3])
     elif window == 100:
-        correction = secure_random.choice([0, 4.2, 4.5, 4.8, 4.2, 4.5, 4.8])
+        correction = secure_random.choice([3.4, 3.6, 3.8, 4.0])
     elif window == 300:
-        correction = secure_random.choice([0, 5.0, 5.3, 5.6, 4.2, 4.5, 4.8])
+        correction = secure_random.choice([3.6, 3.8, 4.0, 4.2, 4.4])
     elif window == 1000:
-        correction = secure_random.choice([0, 6.0, 6.2, 6.4, 4.2, 4.5, 4.8])
+        correction = secure_random.choice([3.8, 4.0, 4.2, 4.4])
 
-    lr = 0.003
+    lr = 0.01
     #print(f'/usr/bin/Rscript sample_code.r {sd} {window} {correction} {seed} > output/output_sd_{sd}_window_{window}_corr_{correction}_seed_{seed}')
     os.system(f'/usr/bin/Rscript sample_code.r {sd} {window} {correction} {seed} {lr} > output/output_lr_{lr}_sd_{sd}_window_{window}_corr_{correction}_seed_{seed}')
