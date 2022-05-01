ca9021b8ad96f172be9e659644b1e2c63a444a4f
Wei Deng <greyman@live.cn>
2022-05-01 14:17:13 -0400

    .

--- simulation_corrections/grid_search.py
+++ simulation_corrections/grid_search.py
@@ -7,12 +7,24 @@ import sys
 secure_random = random.SystemRandom()
 
 
-for _ in range(1):
+for _ in range(10):
     seed = str(random.randint(1, 10**5))
     sd = secure_random.choice([4])
-    window = secure_random.choice([3])
-    correction = secure_random.choice([2.0, 2.1, 2.2, 2.3, 2.4, 2.5])
-    
+    window = secure_random.choice([1, 3, 10, 30, 100, 300, 1000])
+    if window == 1:
+        correction = 0
+    elif window == 3:
+        correction = secure_random.choice([0, 2.0, 2.2, 2.4, 2.0, 2.2, 2.4])
+    elif window == 10:
+        correction = secure_random.choice([0, 2.0, 2.3, 2.6, 2.0, 2.3, 2.6])
+    elif window == 30:
+        correction = secure_random.choice([0, 3.0, 3.3, 3.6, 3.0, 3.3, 3.6])
+    elif window == 100:
+        correction = secure_random.choice([0, 4.2, 4.5, 4.8, 4.2, 4.5, 4.8])
+    elif window == 300:
+        correction = secure_random.choice([0, 5.0, 5.3, 5.6, 4.2, 4.5, 4.8])
+    elif window == 1000:
+        correction = secure_random.choice([0, 6.0, 6.2, 6.4, 4.2, 4.5, 4.8])
 
     #print(f'/usr/bin/Rscript sample_code.r {sd} {window} {correction} {seed} > output/output_sd_{sd}_window_{window}_corr_{correction}_seed_{seed}')
     os.system(f'/usr/bin/Rscript sample_code.r {sd} {window} {correction} {seed} > output/output_sd_{sd}_window_{window}_corr_{correction}_seed_{seed}')
