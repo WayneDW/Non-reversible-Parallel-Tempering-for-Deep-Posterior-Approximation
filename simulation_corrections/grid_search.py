43571b7c3f37741288aa1ba5f18ebbe4e563eabd
Wei Deng <greyman@live.cn>
2022-05-01 20:56:38 -0400

    .

--- simulation_corrections/grid_search.py
+++ simulation_corrections/grid_search.py
@@ -7,7 +7,7 @@ import sys
 secure_random = random.SystemRandom()
 
 
-for _ in range(3):
+for _ in range(5):
     seed = str(random.randint(1, 10**5))
     sd = secure_random.choice([4])
     window = secure_random.choice([3, 10, 30, 100, 300, 1000])
@@ -18,13 +18,13 @@ for _ in range(3):
     elif window == 10:
         correction = secure_random.choice([1.7, 2.0, 2.3])
     elif window == 30:
-        correction = secure_random.choice([2.7, 3.0, 3.3])
+        correction = secure_random.choice([2.5, 2.7, 3.0, 3.3])
     elif window == 100:
         correction = secure_random.choice([3.4, 3.6, 3.8, 4.0])
     elif window == 300:
         correction = secure_random.choice([3.6, 3.8, 4.0, 4.2, 4.4])
     elif window == 1000:
-        correction = secure_random.choice([3.8, 4.0, 4.2, 4.4])
+        correction = secure_random.choice([3.8, 4.0, 4.2, 4.4, 4.6])
 
     lr = 0.01
     #print(f'/usr/bin/Rscript sample_code.r {sd} {window} {correction} {seed} > output/output_sd_{sd}_window_{window}_corr_{correction}_seed_{seed}')
