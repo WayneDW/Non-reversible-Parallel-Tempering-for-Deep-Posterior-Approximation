90fdb62a9ad4f40a82cc747b5eb70ed963fb82cd
Wei Deng <greyman@live.cn>
2022-05-06 11:30:16 -0400

    .

--- simulation_corrections/grid_search_sd_conjecture.py
+++ simulation_corrections/grid_search_sd_conjecture.py
@@ -15,19 +15,19 @@ for _ in range(2):
     if window == 1:
         correction = 0
     elif window == 3:
-        correction = secure_random.choice([0.6])
+        correction = secure_random.choice([0.9])
     elif window == 10:
-        correction = secure_random.choice([1.5])
+        correction = secure_random.choice([2.2])
     elif window == 30:
-        correction = secure_random.choice([2.4])
+        correction = secure_random.choice([3.5])
     elif window == 100:
-        correction = secure_random.choice([3.4])
+        correction = secure_random.choice([4.9])
     elif window == 300:
-        correction = secure_random.choice([4.3])
-    elif window == 1000:
-        correction = secure_random.choice([5.2])
-    elif window == 3000:
         correction = secure_random.choice([6.1])
+    elif window == 1000:
+        correction = secure_random.choice([7.5])
+    elif window == 3000:
+        correction = secure_random.choice([8.8])
     #correction = 0
     lr = 0.01
     print(f'/usr/bin/Rscript sample_code_high_t_5.r {sd} {window} {correction} {seed} {lr} > output/output_lr_{lr}_sd_{sd}_window_{window}_corr_{correction}_high_tau_5_seed_{seed}')
