839a60dae416a97bf3f9b181202dfb2ec7664da7
Wei Deng <greyman@live.cn>
2022-05-05 23:01:42 -0400

    .

--- simulation_corrections/grid_search_sd_xxx.py
+++ simulation_corrections/grid_search_sd_xxx.py
@@ -7,12 +7,12 @@ import sys
 secure_random = random.SystemRandom()
 
 
-for _ in range(5):
+for _ in range(1):
     seed = str(random.randint(1, 10**5))
-    sd = secure_random.choice([0, 2, 3, 4, 5, 4, 3, 2, 4, 3, 2, 0])
+    sd = secure_random.choice([0])#, 2, 3, 4, 5, 4, 3, 2, 4, 3, 2, 0])
     window = secure_random.choice([3])
     if sd == 0:
-        correction = secure_random.choice([0.5, 0.7, 0.9])
+        correction = secure_random.choice([0.85, 0.9, 0.95])
     elif sd == 2:
         correction = secure_random.choice([0.55, 0.65, 0.75, 0.9])
     elif sd == 3:
@@ -21,7 +21,7 @@ for _ in range(5):
         correction = secure_random.choice([0.4, 0.45, 0.5, 0.55, 0.65, 0.7, 0.75])
     elif sd == 5:
         correction = secure_random.choice([0.05])
-    correction = 0
+    #correction = 0
     lr = 0.01
     #print(f'/usr/bin/Rscript sample_code.r {sd} {window} {correction} {seed} > output/output_sd_{sd}_window_{window}_corr_{correction}_seed_{seed}')
     os.system(f'/usr/bin/Rscript sample_code.r {sd} {window} {correction} {seed} {lr} > output/output_lr_{lr}_sd_{sd}_window_{window}_corr_{correction}_seed_{seed}')
