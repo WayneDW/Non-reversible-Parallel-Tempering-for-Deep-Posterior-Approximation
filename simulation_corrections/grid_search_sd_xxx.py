d3321e71926fb85d99feab323f8c295362aab503
Wei Deng <greyman@live.cn>
2022-05-06 08:36:13 -0400

    .

--- simulation_corrections/grid_search_sd_xxx.py
+++ simulation_corrections/grid_search_sd_xxx.py
@@ -7,18 +7,18 @@ import sys
 secure_random = random.SystemRandom()
 
 
-for _ in range(10):
+for _ in range(1):
     seed = str(random.randint(1, 10**5))
-    sd = secure_random.choice([0])#, 2, 3, 4, 5, 4, 3, 2, 4, 3, 2, 0])
+    sd = secure_random.choice([4])#, 2, 3, 4, 5, 4, 3, 2, 4, 3, 2, 0])
     window = secure_random.choice([3])
     if sd == 0:
         correction = secure_random.choice([0.55, 0.65, 0.65, 0.7, 0.75, 0.8, 0.85])
     elif sd == 2:
         correction = secure_random.choice([0.55, 0.65, 0.75, 0.9])
     elif sd == 3:
-        correction = secure_random.choice([0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+        correction = secure_random.choice([0.4, 0.5])
     elif sd == 4:
-        correction = secure_random.choice([1.35, 1.4, 1.45])
+        correction = secure_random.choice([0.55, 0.65, 0.65, 0.7, 0.75, 0.8, 0.85])
     elif sd == 5:
         correction = secure_random.choice([0.05])
     #correction = 0
