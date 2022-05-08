6c1f35e00ab225e6bcff280c3d6907015e7478c7
Wei Deng <greyman@live.cn>
2022-05-08 11:02:20 -0400

    .

--- simulation_corrections/grid_search_sd_conjecture.py
+++ simulation_corrections/grid_search_sd_conjecture.py
@@ -10,23 +10,22 @@ secure_random = random.SystemRandom()
 for _ in range(5):
     seed = str(random.randint(1, 10**5))
     sd = secure_random.choice([0])
-    window = secure_random.choice([300, 300, 3000, 3000, 100, 100, 30, 30, 10, 10, 3, 3, 1])
+    window = secure_random.choice([1, 3, 10, 30, 100, 300, 1000, 3000])
     if window == 1:
         correction = 0
     elif window == 3:
-        correction = secure_random.choice([0, 0.7, 0.8, 0.9, 1.0, 1.1])
+        correction = secure_random.choice([0, 0, 0.4, 0.5, 0.6])
     elif window == 10:
-        correction = secure_random.choice([0, 1.8, 1.9, 2.0, 2.1])
+        correction = secure_random.choice([0, 0, 1.3, 1.4, 1.5, 1.6])
     elif window == 30:
-        correction = secure_random.choice([0, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3])
+        correction = secure_random.choice([0, 0, 2.2, 2.3, 2.4, 2.5])
     elif window == 100:
-        correction = secure_random.choice([0, 4.1, 4.2, 4.3, 4.4, 4.5])
+        correction = secure_random.choice([0, 0, 3.4, 3.5, 3.6, 3.7])
     elif window == 300:
-        correction = secure_random.choice([0, 5.2, 5.3])
+        correction = secure_random.choice([0, 0, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9])
     elif window == 1000:
-        correction = secure_random.choice([0, 6.4, 6.5, 6.6, 6.7])
+        correction = secure_random.choice([0, 0, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9])
     elif window == 3000:
-        correction = secure_random.choice([0, 6.9, 7.0, 7.1, 7.2, 7.3])
-    #correction = 0
+        correction = secure_random.choice([0, 0, 6.4, 6.5, 6.6, 6.7, 6.8])
     lr = 0.01
-    os.system(f'/usr/bin/Rscript sample_code_high_t_4.r {sd} {window} {correction} {seed} {lr} > output/output_lr_{lr}_sd_{sd}_window_{window}_corr_{correction}_high_tau_4_seed_{seed}')
+    os.system(f'/usr/bin/Rscript sample_code_high_t_20.r {sd} {window} {correction} {seed} {lr} > output/output_lr_{lr}_sd_{sd}_window_{window}_corr_{correction}_high_tau_20_seed_{seed}')
