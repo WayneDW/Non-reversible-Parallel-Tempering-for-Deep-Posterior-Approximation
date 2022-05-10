4ea384deb2818e15d00936618dee2f8020bd7ee2
Wei Deng <greyman@live.cn>
2022-05-09 23:00:12 -0400

    .

---
+++ simulation_corrections/grid_search_sd_MH.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import random
+import os
+import time
+import sys
+
+secure_random = random.SystemRandom()
+
+
+for _ in range(40):
+    seed = str(random.randint(1, 10**5))
+    sd = secure_random.choice([0])
+    window = secure_random.choice([1, 2, 3, 10, 30, 100, 300, 1000, 3000])
+    if window == 1:
+        correction = 0
+    elif window == 2:
+        correction = secure_random.choice([0.20, 0.22, 0.24, 0.26, 0.28, 0.30, 0.32, 0.34, 0.36, 0.38, 0.40])
+    elif window == 3:
+        correction = secure_random.choice([0.32, 0.34, 0.36, 0.38, 0.40, 0.42, 0.44])
+    elif window == 10:
+        correction = secure_random.choice([0.45, 0.46, 0.47, 0.48, 0.49])
+    elif window == 30:
+        correction = secure_random.choice([0.47, 0.48, 0.49, 0.50])
+    elif window == 100:
+        correction = secure_random.choice([0.48, 0.49, 0.50, 0.51, 0.52])
+    elif window == 300:
+        correction = secure_random.choice([0.50, 0.51, 0.52, 0.53, 0.54, 0.56, 0.58, 0.60])
+    elif window == 1000:
+        correction = secure_random.choice([0.56, 0.58, 0.60, 0.62, 0.64, 0.66, 0.68, 0.70])
+    elif window == 3000:
+        correction = secure_random.choice([0.60, 0.62, 0.64, 0.66, 0.68, 0.70, 0.72, 0.74])
+    sd_proposal = secure_random.choice([1, 2, 3, 4])
+    os.system(f'/usr/bin/Rscript sample_code_MH.r {sd} {window} {correction} {seed} {sd_proposal} > output/output_sd_proposal_{sd_proposal}_sd_{sd}_window_{window}_corr_{correction}_MH_seed_{seed}')
---
+++ simulation_corrections/sample_code_MH.r
@@ -0,0 +1,134 @@
+#!/usr/bin/env Rscript
+myargs = commandArgs(trailingOnly=TRUE)
+
+
+std = as.numeric(myargs[1])
+window = as.numeric(myargs[2])
+correction = as.numeric(myargs[3])
+our_seed = as.numeric(myargs[4])
+sd_proposal = as.numeric(myargs[5])
+
+set.seed(our_seed)
+
+
+mean1 = -4
+mean2 = 3
+f = function(x) return(-log(0.4 * dnorm(x, mean=mean1, sd=0.7) + 0.6 * dnorm(x, mean=mean2, sd=0.5)))
+
+
+std = 1e-5
+nosiy_f = function(x) return(f(x) + rnorm(1, 0, std))
+
+
+# Important hyperparameters
+lr = 0.01
+T_low = 1
+
+# Other hyperparameters
+x_low = 0
+x_high = 0
+part1 = 100
+thinning = 10 # setting lr=0.01 and thinning=1 easily leads to MAE 0.00638/ 0.01265/ 0.00701/ swaps avg: 29989
+
+# You can set a larger value to obtain more stable results.
+total = 10000000
+
+
+################################################### reSGLD ###################################################
+samples_ptsgld = c()
+x_low = 0
+counter = 0
+window = 10
+gate = 1 
+swaps = 0
+for (i in 1:part1) {
+    samples_part = c()
+    for (j in 1:(total/part1)) {
+        if (counter %% window == 0) {
+            gate = 1
+        }
+
+        proposedx = rnorm(1, mean=x_low, sd=sd_proposal)
+
+        A = exp(-nosiy_f(proposedx) + nosiy_f(x_low) - correction)
+                
+        if ((runif(1) < A) & (gate == 1)) {
+            x_low = proposedx
+            gate = 0
+            swaps = swaps + 1
+        }
+        counter = counter + 1
+        if (j %% thinning == 0) {samples_part = c(samples_part, x_low)}
+    }
+    print(i)
+    samples_ptsgld = c(samples_ptsgld, samples_part)
+}
+
+# burn-In
+samples_ptsgld2 = samples_ptsgld[1000: length(samples_ptsgld)]
+
+
+
+part_left = as.integer(length(samples_ptsgld2) *0.4)
+part_right = length(samples_ptsgld2) - part_left
+
+real_samples = c(rnorm(part_left, mean=mean1, sd=0.7), rnorm(part_right, mean=mean2, sd=0.5))
+
+wdata = data.frame(
+        Type = factor(rep(c("Ground truth", "reSGLD"), each=length(samples_ptsgld2))),
+        weight = c(real_samples, samples_ptsgld2)
+        )
+
+print(c("Number of swaps", swaps))
+
+
+
+moving_avg = function(pdf_hist, partition) {
+    # create weight funcs
+    decay = 0.9
+    ww = c(1)
+    width = 10
+    for (idx in 1:width) {
+        ww = c(ww, ww[idx] * decay)
+    }
+    ww = c(rev(ww[2:length(ww)]), ww)
+    # take smoothing average
+    new_pdf = c()
+    for (idx in (width+1): (partition-(width+1))) {
+        smooth_avg = sum(pdf_hist[(idx-width): (idx+width)] * ww) / sum(ww)
+        new_pdf = c(new_pdf, smooth_avg)
+    }
+    new_pdf = c(pdf_hist[1:width], new_pdf, pdf_hist[(length(pdf_hist)-width):length(pdf_hist)])
+    return(new_pdf)
+}
+
+
+histogram_pdf = function(samples) {
+    left = -7
+    right = 6
+    partition = 300
+    delta = (right - left) / partition
+    pdf_hist = c()
+    for (idx in 1:partition) {
+        boundary_left = left + idx * delta
+        boundary_right = left + (idx + 1) * delta
+        count = sum((samples >= boundary_left) & (samples < boundary_right))
+        pdf_hist = c(pdf_hist, count)
+    }
+    pdf_hist = pdf_hist / sum(pdf_hist)
+    pdf_smooth = moving_avg(pdf_hist, partition)
+    #plot(pdf_hist)
+    #lines(pdf_smooth)
+    return(pdf_smooth)
+}
+
+
+real_hist = histogram_pdf(real_samples)
+empi_hist = histogram_pdf(samples_ptsgld2)
+
+
+print(c("Sum of error", sum(abs(real_hist - empi_hist))))
+
+
+
+
