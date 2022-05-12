c4bf36eadfc4ea3524242fb0bdc4421d350cd25b
Wei Deng <greyman@live.cn>
2022-05-12 10:01:11 -0400

    .

--- simulation_corrections/grid_search_sd_6.py
+++ simulation_corrections/grid_search_sd_6.py
@@ -7,26 +7,26 @@ import sys
 secure_random = random.SystemRandom()
 
 
-for _ in range(5):
+for _ in range(1):
     seed = str(random.randint(1, 10**5))
     sd = secure_random.choice([6])
-    window = secure_random.choice([100, 1000, 3000])
+    window = secure_random.choice([100])
     if window == 1:
-        correction = 0
+        correction = 0 # 3.97e-2
     elif window == 3:
-        correction = secure_random.choice([0.3])
+        correction = secure_random.choice([0.3]) # 28/ 3.30e-2 
     elif window == 10:
-        correction = secure_random.choice([0.4])
+        correction = secure_random.choice([0.4]) # 16/ 3.67e-2
     elif window == 30:
-        correction = secure_random.choice([0.5])
+        correction = secure_random.choice([0.5]) # 16/ 3.31e-2
     elif window == 100:
-        correction = secure_random.choice([0.45, 0.55, 0.65, 0.75, 0.85])
+        correction = secure_random.choice([0.9, 0.95, 1.0, 1.05, 1.1, 1.15, 1.20, 1.25, 1.30, 1.35, 1.4]) # 25/ 4.58e-2
     elif window == 300:
-        correction = secure_random.choice([0.7])
+        correction = secure_random.choice([0.7]) # 23/ 4.28e-2
     elif window == 1000:
-        correction = secure_random.choice([1.0, 1.1, 1.2, 1.3, 1.4, 1.5])
+        correction = secure_random.choice([1.6]) # 12/ 3.93e-2
     elif window == 3000:
-        correction = secure_random.choice([1.3, 1.4, 1.5, 1.7, 1.9, 2.1])
+        correction = secure_random.choice([1.7]) # 8/ 4.43e-2
     lr = 0.01
     #print(f'/usr/bin/Rscript sample_code.r {sd} {window} {correction} {seed} > output/output_sd_{sd}_window_{window}_corr_{correction}_seed_{seed}')
     os.system(f'/usr/bin/Rscript sample_code_no_DEO.r {sd} {window} {correction} {seed} {lr} > output/output_lr_{lr}_sd_{sd}_window_{window}_corr_{correction}_no_DEO_seed_{seed}')
