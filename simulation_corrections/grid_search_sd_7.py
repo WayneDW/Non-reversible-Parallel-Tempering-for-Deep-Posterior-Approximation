85b1d9a5772512a3d831bc2c4cea3f508f11e527
Wei Deng <greyman@live.cn>
2022-05-14 07:19:37 -0400

    .

--- simulation_corrections/grid_search_sd_7.py
+++ simulation_corrections/grid_search_sd_7.py
@@ -7,26 +7,26 @@ import sys
 secure_random = random.SystemRandom()
 
 
-for _ in range(10):
+for _ in range(5):
     seed = str(random.randint(1, 10**5))
     sd = secure_random.choice([7])
-    window = secure_random.choice([3, 10, 30, 100])
+    window = secure_random.choice([1, 3, 10, 30, 100, 300, 1000, 3000])
     if window == 1:
         correction = 0
     elif window == 3:
-        correction = secure_random.choice([0.05, 0.10, 0.15, 0.20, 0.25, 0.30])
+        correction = secure_random.choice([0])
     elif window == 10:
-        correction = secure_random.choice([0.10, 0.15, 0.20, 0.25, 0.30, 0.35])
+        correction = secure_random.choice([0])
     elif window == 30:
-        correction = secure_random.choice([0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45])
+        correction = secure_random.choice([0])
     elif window == 100:
-        correction = secure_random.choice([0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55])
+        correction = secure_random.choice([0])
     elif window == 300:
-        correction = secure_random.choice([0.65, 0.90])
+        correction = secure_random.choice([0])
     elif window == 1000:
-        correction = secure_random.choice([0.95])
+        correction = secure_random.choice([0])
     elif window == 3000:
-        correction = secure_random.choice([1.15])
+        correction = secure_random.choice([0])
     lr = 0.005
     #print(f'/usr/bin/Rscript sample_code.r {sd} {window} {correction} {seed} > output/output_sd_{sd}_window_{window}_corr_{correction}_seed_{seed}')
     os.system(f'/usr/bin/Rscript sample_code_no_DEO_sd_7.r {sd} {window} {correction} {seed} {lr} > output/output_lr_{lr}_sd_{sd}_window_{window}_corr_{correction}_sd_7_seed_{seed}')
---
+++ simulation_corrections/output/z_filename_group_avg_no_DEO_sd_7.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import os, sys, re
+from glob import glob
+
+
+group_to_mae = {}
+group_to_swaps = {}
+cnt_to_mae = {}
+for filename in glob('./output*sd*sd_7*'):
+    num_swaps, mae = 0, 0
+    for line in open(filename):
+        line = line.strip()
+        #m = re.match(r'\"Number of swaps\" \"(\d+)\"', line)
+        m = re.match(r'.*Number of swaps" "(.+)"', line)
+        if m:
+            num_swaps = int(m.group(1))
+
+        m = re.match(r'.*Sum of error"(.+)"(.+)"', line)
+        if m:
+            mae = float(m.group(2))
+
+        if num_swaps > 0 and mae > 0:
+            #output_sd_4_window_1_corr_0_seed_2537_swap_30182_MAE_0.034801212762021_end
+            m = re.match(r'.*sd_(.+)_window_(.+)_corr_(.+)_seed_(.+).*', filename)
+            sd = m.group(1)
+            window = m.group(2)
+            corr = m.group(3)
+            feat = f'sd_{sd}_window_{window}_corr_{corr}'
+            if feat not in group_to_mae:
+                group_to_mae[feat], cnt_to_mae[feat] = 0, 0
+                group_to_swaps[feat] = 0
+            group_to_mae[feat] += mae
+            group_to_swaps[feat] += num_swaps
+            cnt_to_mae[feat] += 1
+
+for feat in sorted(cnt_to_mae.keys()):
+    cnt = cnt_to_mae[feat]
+    mae_avg = group_to_mae[feat] / cnt_to_mae[feat]
+    swaps_avg = group_to_swaps[feat] * 1.0 / cnt_to_mae[feat]
+    print(f'{feat} cnt {cnt} MAE {mae_avg: .2e} Swaps {swaps_avg: .1f}')
+
+    
