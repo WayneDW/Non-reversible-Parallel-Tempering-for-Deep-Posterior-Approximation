e35f377ca07da3894d197615c4412595276da369
Wei Deng <greyman@live.cn>
2022-05-14 10:35:55 -0400

    .

--- simulation_corrections/jupyter_notebook_sample.py
+++ simulation_corrections/jupyter_notebook_sample.py
@@ -7,8 +7,6 @@ from autograd import grad
 from scipy.stats import norm
 from scipy.stats import gaussian_kde
 
-#from statistics import NormalDist
-
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 
@@ -23,10 +21,6 @@ def gaussian_density(x, mean, sd):
 def gaussian_mixture_density(x):
     return 0.4 * gaussian_density(x, -4, 0.7) + 0.6 * gaussian_density(x, 3, 0.5)
 
-#def gaussian_mixture_density(x):
-#    return 0.4 * NormalDist(mu=-4, sigma=0.7).pdf(x) + 0.6 * NormalDist(mu=3, sigma=0.5).pdf(x)
-
-
 def energy_function(x):
     return -np.log(gaussian_mixture_density(x))
 
@@ -35,17 +29,17 @@ energy_gradient = grad(energy_function)
 def noisy_energy(x, std): return energy_function(x) + norm.rvs(size=1, loc=0, scale=std)
 
 class PT_sampler:
-    def __init__(self, lr=0.01, window=1, window_correction=0, std=0, T=[1, 10], num_samples=100000):
+    def __init__(self, lr=0.01, window=1, w_correction=0, std=0, T=[1, 10], num_samples=100000):
         self.lr = lr
         self.T_low, self.T_high = T
         self.x_low, self.x_high = 0., 0.
         self.num_samples, self.thinning = num_samples, 10
         
-        self.window, self.w_correction = window, window_correction
+        self.window, self.w_correction = window, w_correction
         self.std = std
         
         self.swaps, self.gate = 0, 1
-        print(f'lr {lr} window {window} w-correction {window_correction} sd of energy {std}')
+        print(f'lr {lr} window {window} w-correction {w_correction} sd of energy {std}')
         self._solver()
         
     def _solver(self):
@@ -80,21 +74,26 @@ class PT_sampler:
         """ remove burn-in steps """
         self.samples = self.samples[-num_samples: ]
 
+sampler_PT = PT_sampler(window=1, w_correction=0, std=0, num_samples=num_samples)
 
-#num_samples = 10000
+sampler_PT_100_38_0 = PT_sampler(window=100, w_correction=3.8, std=0, num_samples=num_samples)
+sampler_PT_100_33_0 = PT_sampler(window=100, w_correction=3.3, std=0, num_samples=num_samples)
+sampler_PT_100_28_0 = PT_sampler(window=100, w_correction=2.8, std=0, num_samples=num_samples)
 
-sampler_PT = PT_sampler(window=1, window_correction=0, std=0, num_samples=num_samples)
-
-sampler_PT_100_38_0 = PT_sampler(window=100, window_correction=3.8, std=0, num_samples=num_samples)
 
 real_samples = norm.rvs(size=int(num_samples*0.4), loc=-4, scale=0.7).tolist() + \
                norm.rvs(size=int(num_samples*0.6), loc=3, scale=0.5).tolist()
 
 x_ticks = np.arange(-6, 5, 0.1)
 
-
 MAE = np.mean(np.abs(gaussian_kde(real_samples)(x_ticks) - gaussian_kde(sampler_PT.samples)(x_ticks)))
 print(f'MAE {MAE: .2e}')
 
 MAE = np.mean(np.abs(gaussian_kde(real_samples)(x_ticks) - gaussian_kde(sampler_PT_100_38_0.samples)(x_ticks)))
+print(f'MAE {MAE: .2e}')
+
+MAE = np.mean(np.abs(gaussian_kde(real_samples)(x_ticks) - gaussian_kde(sampler_PT_100_33_0.samples)(x_ticks)))
+print(f'MAE {MAE: .2e}')
+
+MAE = np.mean(np.abs(gaussian_kde(real_samples)(x_ticks) - gaussian_kde(sampler_PT_100_28_0.samples)(x_ticks)))
 print(f'MAE {MAE: .2e}')
